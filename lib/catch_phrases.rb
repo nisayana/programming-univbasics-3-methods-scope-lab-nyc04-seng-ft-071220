6f6fa3f715152188dd8b988fcab37c0281a80576
Sayana <nisayana23@yandex.ru>
2020-07-16 15:09:06 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,6 +1,3 @@
-
-
-
 def mario
   phrase  = "It's-a me, Mario!"
   puts phrase
@@ -9,4 +6,8 @@ end
 def toadstool
   status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
+end
+
+def link
+  puts "It's Dangerous To Go Alone! Take This."
 end
